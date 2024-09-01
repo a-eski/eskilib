dc849dd68ceb57735beac3d0c3a8f3eef513853c
Alex Eski <alex.eski@tapcheck.com>
2024-09-01 08:03:03 -0700

    switch hashtable to list's style

--- src/eskilib_hashtable.h
+++ src/eskilib_hashtable.h
@@ -1,17 +1,27 @@
 #ifndef eskilib_hashtable_h
 #define eskilib_hashtable_h
 
+enum eskilib_HashTable_Result
+{
+	FAILURE_ZERO_KEY_VALUE = -4,
+	FAILURE_OVERFLOW_PROTECTION = -3,
+	FAILURE_NULL_ENTRY = -2,
+	FAILURE_NULL_HASHTABLE = -1,
+	FAILURE = 0,
+	SUCCESS = 1
+};
+
 typedef struct
 {
-	size_t Key;
-	void* Value;
+	size_t key;
+	void* value;
 } eskilib_HashTable_Entry;
 
 typedef struct
 {
-	size_t Size;
-	size_t Position;
-	eskilib_HashTable_Entry* Entries;
+	size_t size;
+	size_t position;
+	eskilib_HashTable_Entry* entries;
 } eskilib_HashTable;
 
 eskilib_HashTable* eskilib_hashtable_allocate(const size_t sizeOfHashTable, const size_t sizeOfValues);
@@ -20,7 +30,7 @@ void eskilib_hashtable_free(eskilib_HashTable* hashtable);
 
 size_t eskilib_hashtable_add(void* value, eskilib_HashTable* hashtable);
 
-bool eskilib_hashtable_remove(const size_t key, eskilib_HashTable* hashtable);
+enum eskilib_HashTable_Result eskilib_hashtable_remove(const size_t key, eskilib_HashTable* hashtable);
 
 eskilib_HashTable_Entry* eskilib_hashtable_get(const size_t key, eskilib_HashTable* hashtable);
 
