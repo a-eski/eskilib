ff9019cc16ff7f7fd06baacdf0e8c0b456f834e4
Alex Eski <alexeski@gmail.com>
2024-09-06 05:58:39 -0700

    remove local work

--- src/inline/eskilib_error_handler.h
+++
@@ -1,9 +0,0 @@
-#ifndef eskilib_error_handler_h
-#define eskilib_error_handler_h
-
-void eskilib_output_allocation_error_and_exit(char* errorMessage);
-
-void eskilib_output_allocation_error_and_execute(char* errorMessage, void (*function)());
-
-#endif /* !eskilib_error_handler_h */
-
--- src/inline/eskilib_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef eskilib_test_h
-#define eskilib_test_h
-
-#define eskilib_assert(condition) if (!(condition)) printf("%s condition failed on line %d", __func__, __LINE__);
-
-void eskilib_test_run(const char* function_name, void (*function)());
-
-#endif /* !eskilib_test_h */
-
