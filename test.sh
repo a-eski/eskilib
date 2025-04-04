deb5ac06763cf47c695f2f9bb8adc6a1ee7162e7
Alex Eski <alexeski@gmail.com>
2025-04-04 09:30:15 -0700

    switch back to gcc

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-clang-19 -std=c2x -Wall -Wextra -Werror -Wpedantic -pedantic-errors -Wconversion -Wsign-conversion -Wformat=2 -Wshadow -Wvla -fstack-protector-all -Detest_ALL -DNDEBUG src/*.c src/tests/*.c src/data_structure_templates/tests/*.c -o bin/etests
+gcc -std=c2x -Wall -Wextra -Werror -Wpedantic -pedantic-errors -Wconversion -Wsign-conversion -Wformat=2 -Wshadow -Wvla -fstack-protector-all -Detest_ALL -DNDEBUG src/*.c src/tests/*.c src/data_structure_templates/tests/*.c -o bin/etests
 bin/etests
